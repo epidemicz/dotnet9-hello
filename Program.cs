adbb751627b5c97ca8dae176843c8ef4d7fb9a91
Tim Zenner <epidemicz@gmail.com>
2025-01-11 14:52:21 -0600

    added args ouput

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("args:");
+
+var i = 0;
+foreach (var a in args)
+{
+    Console.WriteLine($"arg[{i++}]: {a}");
+}
